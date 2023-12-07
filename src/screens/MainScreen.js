1d334a85a722535fb36528fa0f3cfe20cf54eb5d
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-12-07 16:21:35 +0300

    feat:source dir

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function MainScreen() {
+  return (
+    <SafeAreaView>
+      <Text>MainScreen</Text>
+    </SafeAreaView>
+  );
+}
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function ProfileScreen() {
+  return (
+    <SafeAreaView>
+      <Text>ProfileScreen</Text>
+    </SafeAreaView>
+  );
+}
