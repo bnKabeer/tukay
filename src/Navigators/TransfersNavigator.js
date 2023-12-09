594b21ebbaaf805c49fe3e4ae2bb7fcb47f1ae76
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-12-09 13:27:12 +0300

    fix:fixed top navigation implementation

--- src/Navigators/TransfersNavigator.js
+++ src/Navigators/TransfersNavigator.js
@@ -1,33 +1,57 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import BeggersScreen from "../screens/BeggersScreen";
 import GiversScreen from "../screens/GiversScreen";
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function TransfersNavigator() {
+  const screenWidth = Dimensions.get("window").width;
+  const tabCount = 2;
+  const tabWidth = screenWidth / tabCount - 60;
   return (
-    <View style={styles.conatiner}>
-      <View style={styles.innerContainer}>
-        <Tab.Navigator
-          screenOptions={{
-            tabBarStyle: {
-              backgroundColor: "inherit",
-            },
-            tabBarItemStyle: { color: "#fff" },
-            tabBarActiveTintColor: "#fff",
-          }}
-        >
-          <Tab.Screen name="Beggers" component={BeggersScreen} />
-          <Tab.Screen name="Givers" component={GiversScreen} />
-        </Tab.Navigator>
-      </View>
-    </View>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "#03515D",
+          height: 150,
+          paddingTop: 75,
+          borderBottomLeftRadius: 30,
+          borderBottomRightRadius: 30,
+        },
+        tabBarItemStyle: {
+          borderRadius: 20,
+          width: screenWidth / tabCount - 20,
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: "#fff",
+          height: 3,
+          marginBottom: 35,
+          width: tabWidth,
+          left: (screenWidth / tabCount - tabWidth) / 2,
+          // right: (screenWidth / tabCount - tabWidth) / 4,
+        },
+        tabBarLabelStyle: {
+          fontSize: 16,
+          fontWeight: 600,
+        },
+        tabBarContentContainerStyle: {
+          backgroundColor: "#a4abab33",
+          marginHorizontal: 20,
+          borderRadius: 12,
+          justifyContent: "center",
+        },
+        tabBarActiveTintColor: "#fff",
+      }}
+    >
+      <Tab.Screen name="Beggers" component={BeggersScreen} />
+      <Tab.Screen name="Givers" component={GiversScreen} />
+    </Tab.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
-  conatiner: {
+  container: {
     flex: 1,
     alignItems: "center",
     backgroundColor: "#03515D",
